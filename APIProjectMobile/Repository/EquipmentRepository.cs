1680677545f1f9c3de8b3a025ebb55bf99639b0e
ThongTTT <thethongghgh@gmail.com>
2020-07-18 08:40:35 +0700

    -- test deploye

--- APIProjectMobile/Repository/EquipmentRepository.cs
+++ APIProjectMobile/Repository/EquipmentRepository.cs
@@ -27,7 +27,7 @@ namespace APIProjectMobile.Repository
             equipmentModel.EquipmentQuantity = equipment.EquipmentQuantity;
             equipmentModel.EquipmentStatus = Status.AVAILABLE;
             equipmentModel.EquipmentIsDelete = IsDelete.ACTIVE;
-            // tést api azure
+            //jaklsdjlkajskdjljlkajdkjakjdkdsjbdbkabjkbkbdbsdsa
             _context.TblEquipment.Add(equipmentModel);
             try
             {
